a67aa9dc5f166bcd1c9a95bf67e7ad75dc03f323
Piotr <p.mar.22@gmail.com>
2022-11-21 22:14:26 +0100

    Add finishing game in scoreboard
    Signed-off-by: Piotr <p.mar.22@gmail.com>

--- src/main/java/scoreboard/Scoreboard.java
+++ src/main/java/scoreboard/Scoreboard.java
@@ -1,11 +1,30 @@
 package scoreboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scoreboard {
+    private final List<Game> finishedGames = new ArrayList<>();
+
     public Game startGame(String homeTeam, String awayTeam) {
         return new Game(homeTeam, awayTeam);
     }
 
     public Game updateScore(Game game, Score score) {
+        if (finishedGames.contains(game)) {
+            throw new IllegalStateException("Game has been already finished");
+        }
         return new Game(game.getHomeTeam(), game.getAwayTeam(), score);
+    }
+
+    public void finishGame(Game game) {
+        if (finishedGames.contains(game)) {
+            throw new IllegalStateException("Game has been already finished");
+        }
+        finishedGames.add(game);
+    }
+
+    public List<Game> getSummary() {
+        return new ArrayList<>();
     }
 }
--- src/test/java/scoreboard/ScoreboardTest.java
+++ src/test/java/scoreboard/ScoreboardTest.java
@@ -2,10 +2,9 @@ package scoreboard;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreboardTest {
-
     private static final String HOME_TEAM = "England";
     private static final String AWAY_TEAM = "Spain";
 
@@ -36,4 +35,32 @@ class ScoreboardTest {
         assertEquals(AWAY_TEAM, updatedGame.getAwayTeam());
     }
 
+    @Test
+    void whenFinishGameThenRemoveItFromScoreboard() {
+        var scoreboard = new Scoreboard();
+        var game = scoreboard.startGame(HOME_TEAM, AWAY_TEAM);
+
+        scoreboard.finishGame(game);
+
+        var summary = scoreboard.getSummary();
+        assertFalse(summary.contains(game));
+    }
+
+    @Test
+    void whenFinishAlreadyFinishedGameThenThrowException() {
+        var scoreboard = new Scoreboard();
+        var game = scoreboard.startGame(HOME_TEAM, AWAY_TEAM);
+
+        scoreboard.finishGame(game);
+        assertThrows(IllegalStateException.class, () -> scoreboard.finishGame(game));
+    }
+
+    @Test
+    void whenUpdateScoreForFinishedGameThenThrowException() {
+        var scoreboard = new Scoreboard();
+        var game = scoreboard.startGame(HOME_TEAM, AWAY_TEAM);
+        scoreboard.finishGame(game);
+
+        assertThrows(IllegalStateException.class, () -> scoreboard.updateScore(game, new Score(0, 0)));
+    }
 }
